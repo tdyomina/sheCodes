f342c241841e54ce31a26f516a418ecd41878c9c
tdyomina <tatianadyomina.engl@gmail.com>
2022-10-28 16:59:34 +0300

    bootstrap added

--- react-weather-forecast/src/App.js
+++ react-weather-forecast/src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather</h1>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <h1>Weather</h1>
+        </header>
+        <p>
+          This page was coded by{" "}
+          <a
+            href="https://stellular-beijinho-c2bad9.netlify.app/about.html"
+            target="_blank"
+          >
+            Tatiana
+          </a>
+          , welcome to{" "}
+          <a
+            href="https://github.com/tdyomina/sheCodes/tree/main/react-weather-forecast"
+            target="_blank"
+          >
+            My Git.
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
-
-export default App;
